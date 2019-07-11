086fbde8cda2f61e786c1881b69c1e7c240a07b0
Daniela Gavidia <danielags@fb.com>
2019-07-11 18:09:38 +0100

    Added useful info about setting profile and a link to the bot.

--- app.js
+++ app.js
@@ -215,4 +215,23 @@ function verifyRequestSignature(req, res, buf) {
 // listen for requests :)
 var listener = app.listen(config.port, function() {
   console.log("Your app is listening on port " + listener.address().port);
+
+  if (
+    Object.keys(config.personas).length == 0 &&
+    config.appUrl &&
+    config.verifyToken
+  ) {
+    console.log(
+      "Is this the first time running?\n" +
+        "Make sure to set the both the Messenger profile, persona and webhook by visiting:\n" +
+        config.appUrl +
+        "?profile?mode=all&verify_token=" +
+        config.verifyToken
+    );
+  }
+
+  if (config.pageId) {
+    console.log("Test your app by messaging:");
+    console.log("https://m.me/" + config.pageId);
+  }
 });
