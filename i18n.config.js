fe0d82d83d027f38577cae9da806ed45e9e60034
Jorge Olivares <2464679+jorgeluiso@users.noreply.github.com>
2019-09-21 08:42:06 -0700

    Add 10 languages to the messenger profile (#42)
    Summary:
    #### Is your pull request related to a problem? Please describe
    This solves https://github.com/fbsamples/original-coast-clothing/issues/41
    
    #### Describe the solution you'd like
    Fixing the reference to the available languages in the i18n configuration.
    Pull Request resolved: https://github.com/fbsamples/original-coast-clothing/pull/42
    
    Test Plan:
    Deleted my staging messenger profile
    ```
    curl -X DELETE \
      'https://graph.facebook.com/v4.0/me/messenger_profile?fields=[%22get_started%22,%22greeting%22,%22persistent_menu%22]' \
      -H 'Authorization: Bearer XXXXXX'
    ```
    Verified it was empty
    ```
    curl -X GET \
      'https://graph.facebook.com/v4.0/me/messenger_profile?fields=get_started,greeting,persistent_menu' \
      -H 'Authorization: Bearer XXXXXX' \
    ```
    Set the profile from my staging
    ```
    https://xxxxxx.herokuapp.com/profile?mode=all&verify_token=xxxxxxxx
    ```
    
    Query the messenger profile
    ```
    curl -X GET \
      'https://graph.facebook.com/v4.0/me/messenger_profile?fields=get_started,greeting,persistent_menu' \
      -H 'Authorization: Bearer XXXXXX' \
    ```
    Verified it was set for   locales: [
        "en_US",
        "fr_FR",
        "es_ES",
        "es_LA",
        "pt_BR",
        "id_ID",
        "ar_AR",
        "de_DE",
        "it_IT",
        "ja_JP",
        "ko_KR",
        "ru_RU",
        "th_TH",
        "vi_VN",
        "zh_CN",
        "zh_HK",
        "zh_TW"
      ],
    
    Reviewed By: virtuallyawake
    
    Differential Revision: D17494071
    
    Pulled By: jorgeluiso
    
    fbshipit-source-id: e44bfc747747353559f4135f1956782b6da174b6

--- i18n.config.js
+++ i18n.config.js
@@ -12,7 +12,25 @@ const i18n = require("i18n"),
   path = require("path");
 
 i18n.configure({
-  locales: ["en_US", "fr_FR", "es_ES", "es_LA", "pt_BR", "id_ID"],
+  locales: [
+    "en_US",
+    "fr_FR",
+    "es_ES",
+    "es_LA",
+    "pt_BR",
+    "id_ID",
+    "ar_AR",
+    "de_DE",
+    "it_IT",
+    "ja_JP",
+    "ko_KR",
+    "ru_RU",
+    "th_TH",
+    "vi_VN",
+    "zh_CN",
+    "zh_HK",
+    "zh_TW"
+  ],
   defaultLocale: "en_US",
   directory: path.join(__dirname, "locales"),
   objectNotation: true,
