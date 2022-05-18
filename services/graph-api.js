25ced855969145afe89ec37e985d7e13de39611f
Nikita John <nikitajohn@fb.com>
2022-05-17 23:11:14 -0700

    Recurring Notification feature
    Added Recurring Message using token
    
    Added Recurring Message using token and modified the title of Summer Collection
    
    Fixed optin issue
    
    Fixed optin issue

--- services/graph-api.js
+++ services/graph-api.js
@@ -29,7 +29,10 @@ module.exports = class GraphApi {
       body: JSON.stringify(requestBody)
     });
     if (!response.ok) {
-      console.warn(`Could not send message.`, response.statusText);
+      console.warn(
+        `Unable to call Send API: ${response.statusText}`,
+        await response.json()
+      );
     }
   }
 
