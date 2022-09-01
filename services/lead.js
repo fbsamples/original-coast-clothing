9c206e0ad4a82d4ae5f41412d261acb26b74e94a
Jorge Olivares <2464679+jorgeluiso@users.noreply.github.com>
2022-08-31 18:27:54 -0700

    Add delay to lead

--- services/lead.js
+++ services/lead.js
@@ -8,41 +8,43 @@
  * https://developers.facebook.com/docs/messenger-platform/getting-started/sample-apps/original-coast-clothing
  */
 
- "use strict";
+"use strict";
 
- // Imports dependencies
- const Response = require("./response"),
-   //Survey = require("./survey"),
-   config = require("./config"),
-   i18n = require("../i18n.config");
- 
- module.exports = class Lead {
-   constructor(user, webhookEvent) {
-     this.user = user;
-     this.webhookEvent = webhookEvent;
-   }
- 
-   handleHandover(metadata) {
-     switch (metadata) {
-       case "messenger_lead_gen_complete":
-        return [
-            Response.genTextWithPersona(
-              i18n.__("wholesale_leadgen.intro", {
-                userFirstName: this.user.firstName,
-                agentFirstName: config.personaSales.name
-              }),
-              config.personaSales.id
-            ),
-            Response.genTextWithPersona(
-              i18n.__("care.end"),
-              config.personaSales.id
-            ),
-          ];
-       case "messenger_lead_gen_incomplete":
-         return Response.genNuxMessage(this.user);
-     }
- 
-     return;
-   }
- };
- 
+// Imports dependencies
+const Response = require("./response"),
+  //Survey = require("./survey"),
+  config = require("./config"),
+  i18n = require("../i18n.config");
+
+module.exports = class Lead {
+  constructor(user, webhookEvent) {
+    this.user = user;
+    this.webhookEvent = webhookEvent;
+  }
+
+  handleHandover(metadata) {
+    switch (metadata) {
+      case "messenger_lead_gen_complete":
+        let responses = [
+          Response.genTextWithPersona(
+            i18n.__("wholesale_leadgen.intro", {
+              userFirstName: this.user.firstName,
+              agentFirstName: config.personaSales.name,
+            }),
+            config.personaSales.id
+          ),
+          Response.genTextWithPersona(
+            i18n.__("care.end"),
+            config.personaSales.id
+          ),
+        ];
+        responses[0].delay = 4000;
+        responses[1].delay = 1000;
+        return responses;
+      case "messenger_lead_gen_incomplete":
+        return Response.genNuxMessage(this.user);
+    }
+
+    return;
+  }
+};
